acaa31d632dfbbfdd1fa3a0a5ad64b36034fbf5d
Zia <ziabinumer@gmail.com>
2025-09-23 01:50:20 +0500

    fix overflow and underflow

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -9,8 +9,20 @@ class Solution {
         int newNum = 0;
 
         while (x != 0) {
-            newNum = newNum * 10 + x % 10;
+            int digit = x % 10;
             x /= 10;
+
+            // to ensure the newNum stays between the standard int range
+            if (newNum > Integer.MAX_VALUE
+                || newNum == Integer.MAX_VALUE && digit > 7) {
+                    return 0;
+                }
+            if (newNum < Integer.MIN_VALUE 
+                || newNum == Integer.MIN_VALUE && digit < -8) {
+                    return 0;
+                }
+
+            newNum = newNum * 10 + digit;
         }
 
 
