69ff0326ea8c7999548eeced008f309e87203761
Zia <ziabinumer@gmail.com>
2025-10-01 21:15:27 +0500

    Fix type

--- TriangularSumOfArray.java
+++ TriangularSumOfArray.java
@@ -1,18 +1,24 @@
+import java.util.Arrays;
+
 public class TriangularSumOfArray {
     public static void main(String args[]) {
+        int[] nums = {5};
         Solution sol = new Solution();
-        System.out.println(sol.triangularSum(null));
+        System.out.println(sol.triangularSum(nums));
     }
 }
 
 class Solution {
     public int triangularSum(int[] nums) {
         int result = 0;
-        while (nums.length >= 1) {
-            if (nums.length == 1) return nums[0];
-            for (int i = 0; i < nums.length - 1; i++) {
+        int length = nums.length;
+        while (length >= 1) {
+            if (length == 1) return nums[0];
+            for (int i = 0; i < length - 1; i++) {
                 nums[i] = (nums[i] + nums[i + 1]) % 10;
             }
+            System.out.println(Arrays.toString(nums));
+            length--;
         }
 
         return result;
